ca566137174e8c2c24f54b73cc744223e5356fbb
moobid <simon.irsch@yahoo.de>
2013-09-11 20:19:44 +0200

    Setup

---
+++ gen/com/eps_hioa_2013/JointAttentionResearchApp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.eps_hioa_2013.JointAttentionResearchApp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
