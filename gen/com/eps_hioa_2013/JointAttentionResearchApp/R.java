b8082f1a1ead5ae32c84c20695d8e352f7e76023
moobid <simon.irsch@yahoo.de>
2013-10-14 23:13:11 +0200

    Minor, but needed changes

--- gen/com/eps_hioa_2013/JointAttentionResearchApp/R.java
+++ gen/com/eps_hioa_2013/JointAttentionResearchApp/R.java
@@ -9,20 +9,9 @@ package com.eps_hioa_2013.JointAttentionResearchApp;
 
 public final class R {
     public static final class array {
-        public static final int fruits=0x7f070000;
+        public static final int fruits=0x7f060000;
     }
     public static final class attr {
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarButtonStyle=0x7f010001;
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarStyle=0x7f010000;
-    }
-    public static final class color {
-        public static final int black_overlay=0x7f050000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -31,75 +20,75 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
-        public static final int size12=0x7f060006;
-        public static final int size16=0x7f060005;
-        public static final int size2=0x7f060002;
-        public static final int size24=0x7f060007;
-        public static final int size32=0x7f060008;
-        public static final int size4=0x7f060003;
-        public static final int size48=0x7f060009;
-        public static final int size64=0x7f06000a;
-        public static final int size8=0x7f060004;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int size12=0x7f050006;
+        public static final int size16=0x7f050005;
+        public static final int size2=0x7f050002;
+        public static final int size24=0x7f050007;
+        public static final int size32=0x7f050008;
+        public static final int size4=0x7f050003;
+        public static final int size48=0x7f050009;
+        public static final int size64=0x7f05000a;
+        public static final int size8=0x7f050004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Actions=0x7f0b0031;
-        public static final int Actionstext=0x7f0b0030;
-        public static final int LinearLayout1=0x7f0b0000;
-        public static final int Preactions=0x7f0b002d;
-        public static final int Preactionstext=0x7f0b002c;
-        public static final int Rewards=0x7f0b0033;
-        public static final int Rewardstext=0x7f0b0032;
-        public static final int Signals=0x7f0b002f;
-        public static final int Signalstext=0x7f0b002e;
-        public static final int TableLayout1=0x7f0b000d;
-        public static final int TableRow4=0x7f0b0017;
-        public static final int TextView10=0x7f0b001b;
-        public static final int TextView4=0x7f0b0018;
-        public static final int action_settings=0x7f0b0034;
-        public static final int bottomleft=0x7f0b0007;
-        public static final int bottommid=0x7f0b0008;
-        public static final int bottomright=0x7f0b0009;
-        public static final int button1=0x7f0b0026;
-        public static final int button2=0x7f0b0027;
-        public static final int button3=0x7f0b0029;
-        public static final int date_textview=0x7f0b0019;
-        public static final int editText1=0x7f0b002b;
-        public static final int editText2=0x7f0b002a;
-        public static final int lastused_textview=0x7f0b001c;
-        public static final int lastused_textview2=0x7f0b001e;
-        public static final int lastused_textview3=0x7f0b0020;
-        public static final int lastused_textview4=0x7f0b0021;
-        public static final int midleft=0x7f0b0004;
-        public static final int midmid=0x7f0b0005;
-        public static final int midright=0x7f0b0006;
-        public static final int participant=0x7f0b000b;
-        public static final int participant_textview=0x7f0b0013;
-        public static final int password=0x7f0b000a;
-        public static final int password_textview=0x7f0b0010;
-        public static final int researcher=0x7f0b000c;
-        public static final int researcher_textview=0x7f0b0016;
-        public static final int tableRow1=0x7f0b000e;
-        public static final int tableRow10=0x7f0b001f;
-        public static final int tableRow2=0x7f0b0011;
-        public static final int tableRow20=0x7f0b0028;
-        public static final int tableRow3=0x7f0b0014;
-        public static final int tableRow5=0x7f0b0022;
-        public static final int tableRow6=0x7f0b0024;
-        public static final int tableRow7=0x7f0b0025;
-        public static final int tableRow8=0x7f0b001a;
-        public static final int tableRow9=0x7f0b001d;
-        public static final int textView1=0x7f0b000f;
-        public static final int textView2=0x7f0b0012;
-        public static final int textView3=0x7f0b0015;
-        public static final int textView5=0x7f0b0023;
-        public static final int topleft=0x7f0b0001;
-        public static final int topmid=0x7f0b0002;
-        public static final int topright=0x7f0b0003;
+        public static final int Actions=0x7f0a0031;
+        public static final int Actionstext=0x7f0a0030;
+        public static final int LinearLayout1=0x7f0a0000;
+        public static final int Preactions=0x7f0a002d;
+        public static final int Preactionstext=0x7f0a002c;
+        public static final int Rewards=0x7f0a0033;
+        public static final int Rewardstext=0x7f0a0032;
+        public static final int Signals=0x7f0a002f;
+        public static final int Signalstext=0x7f0a002e;
+        public static final int TableLayout1=0x7f0a000d;
+        public static final int TableRow4=0x7f0a0017;
+        public static final int TextView10=0x7f0a001b;
+        public static final int TextView4=0x7f0a0018;
+        public static final int action_settings=0x7f0a0034;
+        public static final int bottomleft=0x7f0a0007;
+        public static final int bottommid=0x7f0a0008;
+        public static final int bottomright=0x7f0a0009;
+        public static final int button1=0x7f0a0026;
+        public static final int button2=0x7f0a0027;
+        public static final int button3=0x7f0a0029;
+        public static final int date_textview=0x7f0a0019;
+        public static final int editText1=0x7f0a002b;
+        public static final int editText2=0x7f0a002a;
+        public static final int lastused_textview=0x7f0a001c;
+        public static final int lastused_textview2=0x7f0a001e;
+        public static final int lastused_textview3=0x7f0a0020;
+        public static final int lastused_textview4=0x7f0a0021;
+        public static final int midleft=0x7f0a0004;
+        public static final int midmid=0x7f0a0005;
+        public static final int midright=0x7f0a0006;
+        public static final int participant=0x7f0a000b;
+        public static final int participant_textview=0x7f0a0013;
+        public static final int password=0x7f0a000a;
+        public static final int password_textview=0x7f0a0010;
+        public static final int researcher=0x7f0a000c;
+        public static final int researcher_textview=0x7f0a0016;
+        public static final int tableRow1=0x7f0a000e;
+        public static final int tableRow10=0x7f0a001f;
+        public static final int tableRow2=0x7f0a0011;
+        public static final int tableRow20=0x7f0a0028;
+        public static final int tableRow3=0x7f0a0014;
+        public static final int tableRow5=0x7f0a0022;
+        public static final int tableRow6=0x7f0a0024;
+        public static final int tableRow7=0x7f0a0025;
+        public static final int tableRow8=0x7f0a001a;
+        public static final int tableRow9=0x7f0a001d;
+        public static final int textView1=0x7f0a000f;
+        public static final int textView2=0x7f0a0012;
+        public static final int textView3=0x7f0a0015;
+        public static final int textView5=0x7f0a0023;
+        public static final int topleft=0x7f0a0001;
+        public static final int topmid=0x7f0a0002;
+        public static final int topright=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -108,54 +97,46 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_module_settings=0x7f030003;
     }
     public static final class menu {
-        public static final int game_engine=0x7f0a0000;
-        public static final int main=0x7f0a0001;
-        public static final int module=0x7f0a0002;
-        public static final int module_settings=0x7f0a0003;
+        public static final int main=0x7f090000;
+        public static final int module=0x7f090001;
+        public static final int module_settings=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f080001;
-        public static final int app_name=0x7f080000;
-        public static final int button_add_module=0x7f080005;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int button_add_module=0x7f070005;
         /**  Buttons 
          */
-        public static final int button_start=0x7f080004;
-        public static final int date=0x7f080009;
-        public static final int dummy_button=0x7f080012;
-        public static final int dummy_content=0x7f080013;
-        public static final int hello_world=0x7f080002;
+        public static final int button_start=0x7f070004;
+        public static final int date=0x7f070009;
+        public static final int hello_world=0x7f070002;
         /**  for settings screen 
          */
-        public static final int module_description=0x7f08000f;
-        public static final int module_description_key=0x7f080010;
-        public static final int participant=0x7f080007;
-        public static final int participant_text=0x7f08000b;
-        public static final int password=0x7f080006;
-        public static final int password_text=0x7f08000a;
-        public static final int researcher=0x7f080008;
-        public static final int researcher_text=0x7f08000c;
-        public static final int select_module=0x7f08000d;
-        public static final int title_activity_game=0x7f080011;
-        public static final int title_activity_game_engine=0x7f080014;
-        public static final int title_activity_module=0x7f080003;
-        public static final int title_activity_module_settings=0x7f08000e;
+        public static final int module_description=0x7f07000f;
+        public static final int module_description_key=0x7f070010;
+        public static final int participant=0x7f070007;
+        public static final int participant_text=0x7f07000b;
+        public static final int password=0x7f070006;
+        public static final int password_text=0x7f07000a;
+        public static final int researcher=0x7f070008;
+        public static final int researcher_text=0x7f07000c;
+        public static final int select_module=0x7f07000d;
+        public static final int title_activity_module=0x7f070003;
+        public static final int title_activity_module_settings=0x7f07000e;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
-
         
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
-
     
  API 11 theme customizations can go here. 
 
@@ -165,59 +146,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f090000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f090001;
-        public static final int ButtonBar=0x7f090003;
-        public static final int ButtonBarButton=0x7f090004;
-        public static final int FullscreenActionBarStyle=0x7f090005;
-        public static final int FullscreenTheme=0x7f090002;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
     }
-    public static final class styleable {
-        /** 
-         Declare custom theme attributes that allow changing which styles are
-         used for button bars depending on the API level.
-         ?android:attr/buttonBarStyle is new as of API 11 so this is
-         necessary to support previous API levels.
-    
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.eps_hioa_2013.JointAttentionResearchApp:buttonBarButtonStyle}</code></td><td></td></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.eps_hioa_2013.JointAttentionResearchApp:buttonBarStyle}</code></td><td></td></tr>
-           </table>
-           @see #ButtonBarContainerTheme_buttonBarButtonStyle
-           @see #ButtonBarContainerTheme_buttonBarStyle
-         */
-        public static final int[] ButtonBarContainerTheme = {
-            0x7f010000, 0x7f010001
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.eps_hioa_2013.JointAttentionResearchApp.R.attr#buttonBarButtonStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.eps_hioa_2013.JointAttentionResearchApp:buttonBarButtonStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
-        /**
-          <p>This symbol is the offset where the {@link com.eps_hioa_2013.JointAttentionResearchApp.R.attr#buttonBarStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.eps_hioa_2013.JointAttentionResearchApp:buttonBarStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
-    };
 }
