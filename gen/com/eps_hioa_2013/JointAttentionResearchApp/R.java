36bcb974dd29d94277d4db414ba8f96a99fda2a2
moobid <simon.irsch@yahoo.de>
2013-11-13 11:57:30 +0100

    Theo's Changes to Gamelayout

--- gen/com/eps_hioa_2013/JointAttentionResearchApp/R.java
+++ gen/com/eps_hioa_2013/JointAttentionResearchApp/R.java
@@ -35,6 +35,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int withe=0x7f020001;
     }
     public static final class id {
         public static final int Actions=0x7f0a0032;
